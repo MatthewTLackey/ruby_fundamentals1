0726c9d2c53ea7040cab10b64732b0521853fcd0
MatthewTLackey <lackey.matt@gmail.com>
2013-10-22 14:16:47 -0400

    Exercise 3 Complete

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi, #{name}!"
+puts "How old are you?"
+age = gets.chomp.to_i
+year = 2013
+puts "That means you were born in the year #{year - age}."
